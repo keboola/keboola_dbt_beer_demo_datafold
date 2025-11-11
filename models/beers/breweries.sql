de20baeff23801d95c9bcda0037f1c65539e4fc3
MiroCillik <mcillik@gmail.com>
2025-11-11 12:48:45 +0100

    fix models source for remote

--- models/beers/breweries.sql
+++ models/beers/breweries.sql
@@ -10,4 +10,4 @@ SELECT
   trim("state")           AS brewery_state,
   'USA'                 AS brewery_country
 FROM
-  {{ source('WORKSPACE_863912091', 'breweries') }}
+  {{ source('seed_breweries') }}
